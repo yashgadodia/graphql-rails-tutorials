e7ee498626617085bba2d87263edbfed1c83faa6
Yash Gadodia <pirsquare.yash@gmail.com>
2020-11-26 16:45:42 +0800

    Create User model

--- Gemfile
+++ Gemfile
@@ -19,8 +19,9 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
-# add graphql
+#custom gems
 gem 'graphql', '1.9.17'
+gem 'bcrypt', '~> 3.1.13'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_062146) do
+ActiveRecord::Schema.define(version: 2020_11_26_081655) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
