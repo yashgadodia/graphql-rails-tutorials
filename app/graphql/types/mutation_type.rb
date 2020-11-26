387300ec361f8259f7042b348b048a8b7f0749a7
Yash Gadodia <pirsquare.yash@gmail.com>
2020-11-26 16:14:57 +0800

    CreateLink test

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,13 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    # here we are exposing the mutation
+    field :create_link, mutation: Mutations::CreateLink
+    
+    # # TODO: remove me
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World"
+    # end
   end
 end
